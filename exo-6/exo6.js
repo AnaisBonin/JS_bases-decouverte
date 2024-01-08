98f099ed05918e238972a8cc1c0b342cbc84514c
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-08 16:11:53 +0100

    Ex6 - Les objets - section 1

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -1,17 +1,22 @@
 // Cours 8 de Codecademy
-console.log("exo-6");
+console.log('exo-6');
 
 var joeInfo = {
-    name: "Joe's house",
-    rooms: 5,
-    garage: false,
-    bathrooms: 1 + 2,
-    cars: ['Clio', 'Van'],
+  name: "Joe's house",
+  rooms: 5,
+  garage: false,
+  bathrooms: 1 + 2,
+  cars: ['Clio', 'Van'],
 };
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
-
+console.log(joeInfo.cars.length);
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
+joeInfo.bathrooms = 1;
+console.log(joeInfo.bathrooms);
 
-// 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+// 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information.
+
+joeInfo.garage = true;
+console.log(joeInfo.garage);
