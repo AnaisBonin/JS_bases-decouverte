2b007580b9a8e2e411a2f6f2e1d346bc8c83069e
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-08 10:56:51 +0100

    Exo2 section 3

--- exo-2/script.js
+++ exo-2/script.js
@@ -13,3 +13,16 @@ const weekendMessage = `C'est le weeekeeeend !!`;
 const currentDay = myDate.getDay();
 const isWeekDay = currentDay < 6;
 console.log(isWeekDay ? weekDayMessage : weekendMessage)
+
+
+// Exo 2.3
+
+const currentHour = myDate.getHours();
+
+if (isWeekDay) {
+    if (currentDay == 5 && currentHour > 16) {
+        console.log(weekendMessage);
+    } else console.log(weekDayMessage);
+} else {
+    console.log(weekendMessage);
+}
