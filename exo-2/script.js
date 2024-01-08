a39bb526e301ce79d5df37fec4537a8307cf3f25
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-08 10:55:45 +0100

    Exo2 Les conditions, Section 1: mise en place

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,36 @@
 // you can write js here
 console.log('exo-2');
+
+// Exo 2.1
+const myDate = new Date();
+
+const weekDayMessage = 'Allez travailler';
+const weekendMessage = `C'est le weeekeeeend !!`;
+
+// Exo 2.2
+const currentDay = myDate.getDay();
+const isWeekDay = currentDay < 6;
+console.log(isWeekDay ? weekDayMessage : weekendMessage)
+
+// Exo 2.3
+
+const currentHour = myDate.getHours();
+
+if (isWeekDay) {
+    if (currentDay == 5 && currentHour > 16) {
+        console.log(weekendMessage);
+    } else console.log(weekDayMessage);
+} else {
+    console.log(weekendMessage);
+}
+
+// Exo 2.4
+
+if (isWeekDay) {
+    if ((currentDay == 5 && currentHour > 16) || (currentDay == 1 && currentHour < 9)){
+        console.log(weekendMessage);
+    } else { console.log(weekDayMessage); }
+} else {
+    console.log(weekendMessage);
+}
+
