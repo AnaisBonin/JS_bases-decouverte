48b57757108eb02ae60ffa32e21c6154838ecb5f
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-08 12:11:32 +0100

    Exo3 - Les fonctions / Sections 1 a 3

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,24 @@
-// you can write js here
+// 4 steps :
+// - Récupérer le choix de l'utilisateur
+
+// - Récupérer le choix du programme
+
+// - Comparer les choix
+
+// - Annoncer le gagnant
+
 console.log('exo-3');
+
+let playerInput = prompt('Your choice ( rock, paper, scissors)');
+
+const getPlayerChoice = (playerInput) => {
+  playerInput = playerInput.toLowerCase();
+  const gameOptions = ['rock', 'paper', 'scissors'];
+
+  const isInputValid =
+    gameOptions.find((element) => element == playerInput) != undefined;
+
+  isInputValid ? console.log('valid') : console.log(' nope!');
+};
+
+getPlayerChoice(playerInput);
