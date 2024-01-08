1d23792a64dd4cfca130501b36914c72fd1bd3d0
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-08 14:51:42 +0100

    Exo4 - Les tableaux

--- exo-4/script.js
+++ exo-4/script.js
@@ -2,4 +2,53 @@
 
 console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+var secretMessage = [
+  'Learning',
+  "isn't",
+  'about',
+  'what',
+  'you',
+  'get',
+  'easily',
+  'the',
+  'first',
+  'time,',
+  "it's",
+  'about',
+  'what',
+  'you',
+  'can',
+  'figure',
+  'out.',
+  '-2015,',
+  'Chris',
+  'Pine,',
+  'Learn',
+  'JavaScript',
+];
+
+//1
+secretMessage.pop();
+
+//2
+secretMessage.push('to');
+secretMessage.push('program');
+
+//3
+const easilyPosition = secretMessage.indexOf('easily');
+secretMessage[easilyPosition] = 'right';
+
+//4
+secretMessage.shift();
+
+//5
+secretMessage.unshift('Programming');
+
+//6
+const getPosition = secretMessage.indexOf('get');
+secretMessage.splice(getPosition, 4);
+secretMessage[getPosition] = 'know';
+
+//7
+const finalMessage = secretMessage.join(' ');
+console.log(finalMessage);
