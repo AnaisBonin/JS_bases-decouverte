6dc7687f749ee322567aba548096df36a1e8900c
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-09 11:08:18 +0100

    Exo7 - 1 a 3

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,23 @@
-console.log("exo-7");
+console.log('exo-7');
+
+/// 7.1 - 7.2
+console.log(jsonDatas);
+
+const types = jsonDatas.map(({ type }) => type);
+console.log(types);
+
+const typesTranslation = {
+  car: 'voiture',
+  house: 'maison',
+  game: 'jeu',
+  videoGame: 'jeux vidéo',
+  show: 'concert',
+};
+
+/// 7.3
+jsonDatas.map((item, i) => {
+  const typeTranslation = typesTranslation[item.type];
+  jsonDatas[i] = { ...item, typeTranslation };
+});
 
 console.log(jsonDatas);
